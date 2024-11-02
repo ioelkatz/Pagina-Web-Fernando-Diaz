525d3e071cfa6d474299a38773725ffdc700d980
Ioel Katz <ioelkatz@gmail.com>
2024-11-02 19:27:27 -0300

    WHATSAPP

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import TopNavbar from "./components/TopNavbar";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
-import WhatsAppButton from "./components/whatsAppButton";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 function App() {
   return (
